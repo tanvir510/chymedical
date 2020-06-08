196aefd89066b4c2a3bdccaa63e648d1e8e27611
Tanvir Ahammad  Chy <tanvirchy.dev@gmail.com>
2020-06-09 01:02:34 +0600

    Feature part code update here

--- src/components/HomeFeatures.vue
+++ src/components/HomeFeatures.vue
@@ -3,11 +3,7 @@
     <div class="container">
       <SectionHeading title="Why Choose Us" subtitle="Our Features" />
       <div class="feature_element">
-        <div
-          class="feature_item"
-          v-for="(feature, index) in features"
-          :key="index"
-        >
+        <div class="feature_item" v-for="(feature, index) in features" :key="index">
           <div class="feature_icon">
             <i :class="feature.icon"></i>
           </div>
@@ -26,13 +22,13 @@ import SectionHeading from "../components/reusable/SectionHeading";
 import features from "../json/feature";
 export default {
   components: {
-    SectionHeading,
+    SectionHeading
   },
   data() {
     return {
-      features: features,
+      features: features
     };
-  },
+  }
 };
 </script>
 
@@ -43,7 +39,52 @@ export default {
     display: flex;
     flex-wrap: wrap;
     .feature_item {
-      flex: 1 1 33.33%;
+      flex: 33.33%;
+      padding: 10px;
+      box-sizing: border-box;
+      display: flex;
+      margin-bottom: 25px;
+      &:nth-last-child(-n + 3) {
+        margin-bottom: 0px;
+        padding-bottom: 0px;
+      }
+      &:hover {
+        .feature_icon {
+          i {
+            box-shadow: 0px 0px 0px 1px #2fa5eb inset;
+            color: #2fa5eb;
+          }
+        }
+      }
+      .feature_icon {
+        margin-right: 20px;
+        i {
+          font-size: 36px;
+          width: 60px;
+          height: 60px;
+          line-height: 60px;
+          text-align: center;
+          box-shadow: 0px 0px 0px 46px #2fa5eb inset;
+          color: #fff;
+          border-radius: 4px;
+          display: inline-block;
+          transition: all ease-in-out 0.4s;
+        }
+      }
+      .feature_content {
+        .title {
+          font-size: 18px;
+          margin-bottom: 8px;
+          color: #384047;
+        }
+        .desc {
+          font-size: 14px;
+          color: #717f86;
+          margin-top: 0px;
+          line-height: 21px;
+          margin-bottom: 0px;
+        }
+      }
     }
   }
 }
--- src/json/feature.js
+++ src/json/feature.js
@@ -1,36 +1,36 @@
 const features = [
   {
-    icon: "medical-icon-internal-medicine",
+    icon: "medical-icon-i-internal-medicine",
     title: "Internal Medicine",
     desc:
       "Proin nec lacus lacus. Sed vitae consectetur est, et mattis felis. Fusce lacinia nibh vitae fermentum finibus.",
   },
   {
-    icon: "medical-icon-respiratory",
+    icon: "medical-icon-i-respiratory",
     title: "Respiratory",
     desc:
       "Proin nec lacus lacus. Sed vitae consectetur est, et mattis felis. Fusce lacinia nibh vitae fermentum finibus.",
   },
   {
-    icon: "medical-icon-neurology",
+    icon: "medical-icon-i-neurology",
     title: "Neurology",
     desc:
       "Proin nec lacus lacus. Sed vitae consectetur est, et mattis felis. Fusce lacinia nibh vitae fermentum finibus.",
   },
   {
-    icon: "medical-icon-genetics",
+    icon: "medical-icon-i-genetics",
     title: "Genetics Service",
     desc:
       "Proin nec lacus lacus. Sed vitae consectetur est, et mattis felis. Fusce lacinia nibh vitae fermentum finibus.",
   },
   {
-    icon: "medical-icon-oncology",
+    icon: "medical-icon-i-oncology",
     title: "Oncology Care",
     desc:
       "Proin nec lacus lacus. Sed vitae consectetur est, et mattis felis. Fusce lacinia nibh vitae fermentum finibus.",
   },
   {
-    icon: "medical-icon-dental",
+    icon: "medical-icon-i-dental",
     title: "Dental Care",
     desc:
       "Proin nec lacus lacus. Sed vitae consectetur est, et mattis felis. Fusce lacinia nibh vitae fermentum finibus.",
